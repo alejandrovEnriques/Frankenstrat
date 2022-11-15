f3b3a77103c974f7fa1b0dc24fcfc472049bec2d
DESKTOP-C91O3AQ\Aleja <alejandrovenriques@hotmail.com>
2022-11-14 19:25:11 -0600

    Clean

--- nodes/vector_product.py
+++ nodes/vector_product.py
@@ -10,33 +10,44 @@ importlib.reload(data_plug)
 importlib.reload(double3_plug)
 
 
-class PlusMinusAverage(depend_node.DependNode):
-    _maya_type = "plusminusaverage"
+class VectorProduct(depend_node.DependNode):
+    _maya_type = "vectorProduct"
 
     def __init__(self, name):
-        super(PlusMinusAverage, self).__init__(name)
+        super(VectorProduct, self).__init__(name)
 
-        self._operation = enum_plug.Enum(self._name, "operation", 1, ['No operation', 'Sum', 'Subtract', 'Average'])
+        self._operation = enum_plug.Enum(self._name, "operation", 1, ['No operation', 'Dot Product', 'Cross Product',
+                                                                      'Vector Matrix Product', 'Point Matrix Product'])
         self._output = data_plug.Data(self._name, "output")
 
-        self._input3Dx0 = double_plug.Double(self._name, "input3D[0].input3Dx", 0)
-        self._input3Dy0 = double_plug.Double(self._name, "input3D[0].input3Dy", 0)
-        self._input3Dz0 = double_plug.Double(self._name, "input3D[0].input3Dz", 0)
-        self._input3D0 = double3_plug.Double3(self.name, "input3D[0]",
-                                              [self._input3Dx0, self._input3Dy0, self._input3Dz0])
+        self._input1X = double_plug.Double(self._name, "input1X", 0)
+        self._input1Y = double_plug.Double(self._name, "input1Y", 0)
+        self._input1Z = double_plug.Double(self._name, "input1Z", 0)
+        self._input1 = double3_plug.Double3(self.name, "input1",
+                                              [self._input1X, self._input1Y, self._input1Z])
 
-        self._input3Dx1 = double_plug.Double(self._name, "input3D[1].input3Dx", 0)
-        self._input3Dy1 = double_plug.Double(self._name, "input3D[1].input3Dy", 0)
-        self._input3Dz1 = double_plug.Double(self._name, "input3D[1].input3Dz", 0)
-        self._input3D1 = double3_plug.Double3(self.name, "input3D[1]",
-                                              [self._input3Dx1, self._input3Dy1, self._input3Dz1])
+        self._input2X = double_plug.Double(self._name, "input2X", 0)
+        self._input2Y = double_plug.Double(self._name, "input2Y", 0)
+        self._input2Z = double_plug.Double(self._name, "input2Z", 0)
+        self._input2 = double3_plug.Double3(self.name, "input2",
+                                              [self._input2X, self._input2Y, self._input2Z])
+
+        self._outputX = double_plug.Double(self._name, "outputX", 0)
+        self._outputY = double_plug.Double(self._name, "outputY", 0)
+        self._outputZ = double_plug.Double(self._name, "outputZ", 0)
+        self._output = double3_plug.Double3(self.name, "output",
+                                              [self._outputX, self._outputY, self._outputZ])
+
         self._attributes = [self._operation,
-                            self._input3Dx0,
-                            self._input3Dy0,
-                            self._input3Dz0,
-                            self._input3Dx1,
-                            self._input3Dy1,
-                            self._input3Dz1
+                            self._input1X,
+                            self._input1Y,
+                            self._input1Z,
+                            self._input2X,
+                            self._input2Y,
+                            self._input2Z,
+                            self._outputX,
+                            self._outputY,
+                            self._outputZ
                             ]
 
         self._options = []
@@ -44,43 +55,52 @@ class PlusMinusAverage(depend_node.DependNode):
     @property
     def operation(self):
         return self._operation
+    @property
+    def input1(self):
+        return self._input1
 
     @property
-    def input3D0(self):
-        return self._input3D0
+    def input1X(self):
+        return self._input1X
 
     @property
-    def input3Dx0(self):
-        return self._input3Dx0
+    def input1Y(self):
+        return self._input1Y
 
     @property
-    def input3Dy0(self):
-        return self._input3Dy0
+    def input1Z(self):
+        return self._input1Z
 
     @property
-    def input3Dz0(self):
-        return self._input3Dz0
+    def input2(self):
+        return self._input2
 
     @property
-    def input3D1(self):
-        return self._input3D1
+    def input2X(self):
+        return self._input2X
 
     @property
-    def input3Dx1(self):
-        return self._input3Dx1
+    def input2Y(self):
+        return self._input2Y
 
     @property
-    def input3Dy1(self):
-        return self._input3Dy1
-
-    @property
-    def input3Dz1(self):
-        return self._input3Dz1
-
+    def input2Z(self):
+        return self._input2Z
 
     @property
     def output(self):
         return self._output
+    @property
+    def outputX(self):
+        return self._outputX
+
+    @property
+    def outputY(self):
+        return self._outputY
+
+    @property
+    def outputZ(self):
+        return self._outputZ
 
     def add_option(self, attribute):
         self._options.append(attribute)
